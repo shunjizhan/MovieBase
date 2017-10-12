9b7e9d4bde2cb0bdcb6c7018bc6e90fad5ca16ba
Shunji Zhan <shunhahaha@live.com>
2017-10-12 14:17:34 -0700

    1

--- index.php
+++ index.php
@@ -8,15 +8,17 @@ It is just to give you a rough idea of how your code should work.</p>
 <p>Please do not run a complex query here. You may kill the server. </p>
 Type an SQL query in the following box: <p>
 Example: <tt>SELECT * FROM Actor WHERE id=10;</tt><br />
-<p>
+
 <form action="." method="GET">
 <textarea name="query" cols="60" rows="8"></textarea><br />
 <input type="submit" value="Submit" />
 </form>
-</p>
-<p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small>
-</p>
 
+<p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small></p>
+
+<?php
+echo "My first PHP script!";
+?>
 
 </body>
 </html>
