58efe576e326b2363bbbbca22d28e17fb7960149
Shunji Zhan <shunhahaha@live.com>
2017-10-12 14:09:53 -0700

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head><title>CS143 Project 1B Demo</title></head>
+<body>
+<p>
+Due to security concerns, this demo does not follow the spec exactly.
+It is just to give you a rough idea of how your code should work.</p>
+
+<p>Please do not run a complex query here. You may kill the server. </p>
+Type an SQL query in the following box: <p>
+Example: <tt>SELECT * FROM Actor WHERE id=10;</tt><br />
+<p>
+<form action="." method="GET">
+<textarea name="query" cols="60" rows="8"></textarea><br />
+<input type="submit" value="Submit" />
+</form>
+</p>
+<p><small>Note: tables and fields are case sensitive. All tables in Project 1B are availale.</small>
+</p>
+
+
+</body>
+</html>
