911e726f407f3663b3dd001b6e1ed1942ab1bb18
Shunji Zhan <shunhahaha@live.com>
2017-10-12 15:07:42 -0700

    1

--- create.sql
+++ create.sql
@@ -6,5 +6,16 @@ CREATE TABLE Movie(
 	company varchar(50)
 );
 
+CREATE TABLE Actor();
+CREATE TABLE Sales();
+CREATE TABLE Director();
+CREATE TABLE MovieGenre();
+CREATE TABLE MovieDirector();
+CREATE TABLE MovieActor();
+CREATE TABLE MovieRating();
+CREATE TABLE Review();
+CREATE TABLE MaxPersonID();
+CREATE TABLE MaxMovieID();
+
 LOAD DATA LOCAL INFILE 'www/data/movie.del' INTO TABLE Movie
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
