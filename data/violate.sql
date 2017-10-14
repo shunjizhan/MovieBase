8581c09882edca4c7b744b70dcda6f99fd9f3cc5
Eric <zixia@umail.ucsb.edu>
2017-10-13 18:30:56 -0700

    violate

---
+++ data/violate.sql
@@ -0,0 +1,44 @@
+-- For Movie
+update Movie set title=NULL; -- violates the NOT NULL constraints
+
+insert into	Movie value(489,"Blue Hawaii",1961,"PG","Paramount Pictures"); -- violates the primary key constraints
+
+-- For Actor
+update Actor set dob=NULL; -- violates the NOT NULL constraints
+
+insert into Actor value (65787,"Weinstock","Lotus","Female",19430129,19970831); -- violates the primary key constraints
+
+
+-- For Sales
+insert into Sales value (1111,1174130,17611950); -- violate violates the primary key constraints
+
+insert into Sales value (11111,1174130,17611950); -- violates the foreign key constraints
+
+insert into Sales value (1568,-1,17611950); -- violates the CHECK constrains
+
+-- For Director 
+Update Director set dob=NULL; -- violates the NOT NULL constraints
+
+insert into Director value (3141,"Bahloul","Abdelkrim", NULL,\N); -- violates the primary key constraints
+
+-- For MovieGenre
+insert into MovieGenre value (100000123,"Thriller"); -- violates the foreign key
+
+insert into MovieGenre value (8,"Thriller"); -- violates the primary key constraints
+
+
+-- For MovieActor
+insert into MovieActor value (323200000,5316600000,"Prison Guard"); -- violates the foreign key constraints
+
+
+insert into MovieActor value (3232,53166,"Prison Guard"); -- violates the primary key constraints
+
+-- For MovieRating
+insert into MovieRating value (3561,120,120); --violates the CHECK constrains
+
+insert into MovieRating value (3561,89,33); -- violates the primary key constraints
+
+-- For MovieDirector
+insert into MovieDirector value (219700000,114100000); -- violates the foreigh key
+
+insert into MovieDirector value (2197,1141); -- violates the primary key constraints
