635849cc3ec7c43846b48fc16056207bdb0c5c14
Eric <zixia@umail.ucsb.edu>
2017-10-16 11:36:08 -0700

    team and query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
-SELECT CONCAT( first, ' ', last ) as "name", title, aid title FROM Actor a, Movie m, MovieActor c
+SELECT CONCAT( first, ' ', last ) as "name", title, aid FROM Actor a, Movie m, MovieActor c
 WHERE m.title = 'Death to Smoochy' and c.aid = a.id and m.id = c.mid 
+-- Give the names of all the actors in the movie 'Death to Smoochy' and the tile of movie, and also the id of the Actor.
 
