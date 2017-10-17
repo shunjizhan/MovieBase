0d7fe8b634224f4c5eb8a56c21a123e14321bef1
Shunji Zhan <shunhahaha@live.com>
2017-10-16 20:59:54 -0700

    added console comments to violates.sql

--- violate.sql
+++ violate.sql
@@ -1,47 +1,47 @@
 -- For Movie
-insert into	Movie value(489,NULL,1961,"PG","Paramount Pictures"); -- violates the NOT NULL constraints
+insert into	Movie value(489,NULL,1961,"PG","Paramount Pictures"); -- violates the NOT NULL constraints / Column 'title' cannot be null
 
-insert into	Movie value(489,"Blue Hawaii",1961,"PG","Paramount Pictures"); -- violates the primary key constraints
+insert into	Movie value(489,"Blue Hawaii",1961,"PG","Paramount Pictures"); -- violates the primary key constraints / Duplicate entry '489' for key 'PRIMARY'
 
 -- For Actor
-insert into Actor value (246,"Adams","Edie","Female",NULL,\N);  -- violates the NOT NULL constraints
+insert into Actor value (246,"Adams","Edie","Female",NULL,\N);  -- violates the NOT NULL constraints / Column 'dob' cannot be null
 
-insert into Actor value (246,"Adams","Edie","Female",19270416,\N); -- violates the primary key constraints
+insert into Actor value (246,"Adams","Edie","Female",19270416,\N); -- violates the primary key constraints / Duplicate entry '246' for key 'PRIMARY'
 
 
 
 -- For Sales
-insert into Sales value (1111,1174130,17611950); -- violate violates the primary key constraints
+insert into Sales value (1111,1174130,17611950); -- violate violates the primary key constraints / Duplicate entry '1111' for key 'PRIMARY'
 
-insert into Sales value (11111,1174130,17611950); -- violates the foreign key constraints
+insert into Sales value (11111,1174130,17611950); -- violates the foreign key constraints / Cannot add or update a child row: a foreign key constraint fails (`CS143`.`Sales`, CONSTRAINT `Sales_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
-insert into Sales value (1568,-1,17611950); -- violates the CHECK constrains
+insert into Sales value (1568,-1,-100); -- violates the CHECK constrains
 
 -- For Director 
-insert into Director value (45162,"Neretniece","Ada",NULL,\N); -- violates the NOT NULL constraints
+insert into Director value (45162,"Neretniece","Ada",NULL,\N); -- violates the NOT NULL constraints / Column 'dob' cannot be null
 
-insert into Director value (45162,"Neretniece","Ada",19240602,\N); -- violates the primary key constraints
+insert into Director value (45162,"Neretniece","Ada",19240602,\N); -- violates the primary key constraints / Duplicate entry '45162' for key 'PRIMARY'
 
 -- For MovieGenre
-insert into MovieGenre value (100000123,"Thriller"); -- violates the foreign key
+insert into MovieGenre value (100000123,"Thriller"); -- violates the foreign key / Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieGenre`, CONSTRAINT `MovieGenre_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
-insert into MovieGenre value (8,"Thriller"); -- violates the primary key constraints
+insert into MovieGenre value (8,"Thriller"); -- violates the primary key constraints / Duplicate entry '8' for key 'PRIMARY'
 
 
 -- For MovieActor
-insert into MovieActor value (323200000,5316600000,"Prison Guard"); -- violates the foreign key constraints
+insert into MovieActor value (323200000,5316600000,"Prison Guard"); -- violates the foreign key constraints / Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieActor`, CONSTRAINT `MovieActor_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
 
-insert into MovieActor value (3232,53166,"Prison Guard"); -- violates the primary key constraints
+insert into MovieActor value (3232,53166,"Prison Guard"); -- violates the primary key constraints / Duplicate entry '3232' for key 'PRIMARY'
 
 -- For MovieRating
-insert into MovieRating value (3561,120,120); -- violates the CHECK constrains
+insert into MovieRating value (3561,120,120); -- violates the CHECK constrains / Duplicate entry '3561' for key 'PRIMARY'
 
-insert into MovieRating value (3561,89,33); -- violates the primary key constraints
+insert into MovieRating value (3561,89,33); -- violates the primary key constraints / Duplicate entry '3561' for key 'PRIMARY'
 
 -- For MovieDirector
-insert into MovieDirector value (219700000,114100000); -- violates the foreigh key
+insert into MovieDirector value (219700000,114100000); -- violates the foreigh key / Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieDirector`, CONSTRAINT `MovieDirector_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`))
 
-insert into MovieDirector value (2197,1141); -- violates the primary key constraints
+insert into MovieDirector value (2197,1141); -- violates the primary key constraints Duplicate entry '2197' for key 'PRIMARY'
 
 
