06ca2288993ddf44e0ecaab2c92b1c66d6cdc385
Shunji Zhan <shunhahaha@live.com>
2017-10-24 16:06:19 -0700

    modulized navigatino

---
+++ navigation.php
@@ -0,0 +1,18 @@
+<div class="nav_container">
+  <ul class="nav nav-sidebar">
+    <p>&nbsp;&nbsp;Add new content</p>
+    <li><a href="index.php">Add Actor/Director</a></li>
+    <li><a href="AddMovie.php">Add Movie Information</a></li>
+    <li><a href="Add_M_A_R.php">Add Movie/Actor Relation</a></li>
+    <li><a href="Add_M_D_R.php">Add Movie/Director Relation</a></li>
+  </ul>
+  <ul class="nav nav-sidebar">
+    <p>&nbsp;&nbsp;Browsering Content :</p>
+    <li><a href="Show_A.php">Show Actor Information</a></li>
+    <li><a href="Show_M.php">Show Movie Information</a></li>
+  </ul>
+  <ul class="nav nav-sidebar">
+    <p>&nbsp;&nbsp;Search Interface:</p>
+    <li><a href="search.php">Search/Actor Movie</a></li>
+  </ul>
+</div>
