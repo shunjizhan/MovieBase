d634e08af1e471c3be16d8521e737017e03b69bc
Shunji Zhan <shunhahaha@live.com>
2017-10-29 22:03:48 -0700

    can support multi word search actor

--- show_A.php
+++ show_A.php
@@ -39,13 +39,13 @@
       $search = $_GET["search"];
       $id = $_GET["id"];
 
-      if ($id != NULL) {
+      if (isset($id)) {                   // display actor information
         $query = "select * from Actor where id='{$id}'";
         $result = mysql_query($query, $db);
 
-          $query2 = "select m.title as 'Movie Title', ma.role as 'Role'
-                     from MovieActor ma, Movie m
-                     where ma.aid='{$id}' and ma.mid = m.id";
+          $query2 = "SELECT m.title as 'Movie Title', ma.role as 'Role'
+                     FROM MovieActor ma, Movie m
+                     WHERE ma.aid='{$id}' and ma.mid = m.id";
           $result2 =mysql_query($query2, $db);
 
           print "<h4>Actor's Information is:</h4>";
@@ -54,23 +54,34 @@
           print "<h4>Actor's Movies and Role:</h4>";
           $table2 = new Table($result2, 0);
 
-      } else if($search != NULL) {
+      } else if(isset($search)) {           // show search result
         print "<h4><b>Matching Actors Are:</b></h4>";
 
-        $query = "  SELECT
-        *
-        FROM
-        Actor
-        WHERE
-        last or first
-        LIKE
-        '". mysql_real_escape_string($search) ."%'
-        ";
+        $keywords = explode(" ", mysql_real_escape_string($search));
+        switch(count($keywords)) {
+          case 0:
+          case 1:
+            $query = "SELECT * FROM Actor
+                      WHERE (first LIKE '%$search%') OR (last LIKE '%$search%')";
+            break;
+
+          case 2:
+            $keyword1 = $keywords[0];
+            $keyword2 = $keywords[1];
+            $query = "SELECT * FROM Actor
+                      WHERE (first LIKE '%$keyword1%') AND (last LIKE '%$keyword2%') OR
+                            (last LIKE '%$keyword1%') AND (first LIKE '%$keyword2%')";
+            break;
+
+          default:
+            $query = "SELECT * FROM Actor WHERE FALSE";
+        }
 
         $result = mysql_query($query, $db);
 
         $table = new Table($result, 1);
       }
+
       mysql_close($db);
     ?>
 
