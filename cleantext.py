15f29d20c0bf796d57f53f03517b1dadc57a4862
Don <donlee940125@gmail.com>
2018-05-21 12:18:58 -0700

    grams done

--- cleantext.py
+++ cleantext.py
@@ -133,10 +133,17 @@ def sanitize(text):
 
     # 4. Separate all external punctuation such as periods, commas, etc. into their own tokens (a token is a single piece of text with no spaces), but maintain punctuation within words (otherwise he'll gets parsed to hell and thirty-two gets parsed to thirtytwo). The phrase "The lazy fox, jumps over the lazy dog." should parse to "the lazy fox , jumps over the lazy dog ."
     for index, word in enumerate(splitted_text):
-        if word[-1] in [',', '.', ';', '?', ':', '!'] and len(word) > 1:
+
+        if len(word) > 3 and word[-3:] == '...':
+            word1, word2 = word[:-3], word[-3:]
+            splitted_text[index] = word1
+            splitted_text.insert(index+1, word2)
+
+        elif word[-1] in [',', '.', ';', '?', ':', '!'] and len(word) > 1 and word[0] not in [',', '.', ';', '?', ':', '!']:
             word1, word2 = word[:-1], word[-1]
             splitted_text[index] = word1
             splitted_text.insert(index+1, word2)
+
     # 5. Remove all punctuation (including special characters that are not technically punctuation) except punctuation that ends a phrase or sentence and except embedded punctuation (so thirty-two remains intact). Common punctuation for ending sentences are the period (.), exclamation point (!), question mark (?). Common punctuation for ending phrases are the comma (,), semicolon (;), colon (:). While quotation marks and parentheses also start and end phrases, we will ignore them as it can get complicated. We can also RRR's favorite em-dash (--) as it varies (two hyphens, one hyphen, one dash, two dashes or an em-dash).
 
 
@@ -145,20 +152,42 @@ def sanitize(text):
     # 7. The order of these operations matters, but you are free to experiment and you may get the same results.
 
 
-    # YOUR CODE GOES BELOW:
+    # puarsed text
     new_str = ""
     for splitted in splitted_text:
         new_str = new_str + splitted + " "
-    text = new_str
+    parsed_text = new_str[:-1]
 
-    return text
+    # unigrams
+    new_str = ""
+    for splitted in splitted_text:
+        if not splitted in [',', '.', ';', '?', ':', '!', '...']: 
+            new_str = new_str + splitted + " "
+    unigrams = new_str[:-1]
+
+
+    # bigrams
+    new_str = ""
+    for i in range( len(splitted_text) -1):
+        if (not splitted_text[i] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+1] in [',', '.', ';', '?', ':', '!', '...']):
+            new_str = new_str + splitted_text[i] + "_" + splitted_text[i+1] + " "
+    bigrams = new_str[:-1]
+
+    # trigrams
+    new_str = ""
+    for i in range( len(splitted_text) -2):
+        if (not splitted_text[i] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+1] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+2] in [',', '.', ';', '?', ':', '!', '...']):
+            new_str = new_str + splitted_text[i] + "_" + splitted_text[i+1] + "_" + splitted_text[i+2] + " "
+    trigrams = new_str[:-1]
+
+    return parsed_text, unigrams, bigrams, trigrams
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     print (filename)
 
-    lines_to_read = 10
+    lines_to_read = 1
     with open(filename) as fp:
         for _ in range(lines_to_read):
             line = fp.readline()
