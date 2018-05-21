46903fad3a6f78ad0011327403c53445d4c3e6e7
Eric <zixia@umail.ucsb.edu>
2018-05-21 13:06:59 -0700

    step 456

--- cleantext.py
+++ cleantext.py
@@ -117,7 +117,7 @@ def sanitize(text):
     3. The bigrams
     4. The trigrams
     """
-
+    temp = text
     # 1. Replace new lines and tab characters with a single space.
     text = text.replace("\t", " ")
     text = text.replace("\n", " ")
@@ -132,6 +132,7 @@ def sanitize(text):
     text = re.sub(r'\[.*\]\(https?://\S*\s|\Z', '', text)
     text = re.sub(r'https?://\S*\s|\Z', '', text)
 
+    punctuation = {',', '.', ';', '?', ':', '!', '"', '*'}
     # 3. Split text on a single space. If there are multiple contiguous spaces, you will need to remove empty tokens after doing the split.
     splitted_text = text.split()
     for index, word in enumerate(splitted_text):
@@ -141,15 +142,32 @@ def sanitize(text):
     # 4. Separate all external punctuation such as periods, commas, etc. into their own tokens (a token is a single piece of text with no spaces), but maintain punctuation within words (otherwise he'll gets parsed to hell and thirty-two gets parsed to thirtytwo). The phrase "The lazy fox, jumps over the lazy dog." should parse to "the lazy fox , jumps over the lazy dog ."
     for index, word in enumerate(splitted_text):
 
-        if len(word) > 3 and word[-3:] == '...':
-            word1, word2 = word[:-3], word[-3:]
-            splitted_text[index] = word1
-            splitted_text.insert(index+1, word2)
+        # if len(word) > 3 and word[-3:] == '...':
+        #     word1, word2 = word[:-3], word[-3:]
+        #     splitted_text[index] = word1
+        #     splitted_text.insert(index+1, word2)
 
-        elif word[-1] in [',', '.', ';', '?', ':', '!'] and len(word) > 1 and word[0] not in [',', '.', ';', '?', ':', '!']:
-            word1, word2 = word[:-1], word[-1]
-            splitted_text[index] = word1
-            splitted_text.insert(index+1, word2)
+        # elif word[-1] in punctuation and len(word) > 1 and word[0] not in punctuation:
+        #     word1, word2 = word[:-1], word[-1]
+        #     splitted_text[index] = word1
+        #     splitted_text.insert(index+1, word2)
+        # print(index)
+        # print(word)
+        # print(len(splitted_text))
+        for i, char in enumerate(word):
+            if char in punctuation:
+                if i == 0:
+                    word1 = char
+                    word2 = word[1:]
+                else:
+                    word1, word2 = word[:i], word[i:]
+                splitted_text[index] = word1
+                if word2 != '':
+                    splitted_text.insert(index+1, word2)
+
+                break
+
+
 
     # 5. Remove all punctuation (including special characters that are not technically punctuation) except punctuation that ends a phrase or sentence and except embedded punctuation (so thirty-two remains intact). Common punctuation for ending sentences are the period (.), exclamation point (!), question mark (?). Common punctuation for ending phrases are the comma (,), semicolon (;), colon (:). While quotation marks and parentheses also start and end phrases, we will ignore them as it can get complicated. We can also RRR's favorite em-dash (--) as it varies (two hyphens, one hyphen, one dash, two dashes or an em-dash).
 
@@ -158,17 +176,18 @@ def sanitize(text):
     text = text.lower()
     # 7. The order of these operations matters, but you are free to experiment and you may get the same results.
 
+    parsed_text = " ".join(splitted_text)
 
     # puarsed text
-    new_str = ""
-    for splitted in splitted_text:
-        new_str = new_str + splitted + " "
-    parsed_text = new_str[:-1]
+    # new_str = ""
+    # for splitted in splitted_text:
+    #     new_str = new_str + splitted + " "
+    # parsed_text = new_str[:-1]
 
     # unigrams
     new_str = ""
     for splitted in splitted_text:
-        if not splitted in [',', '.', ';', '?', ':', '!', '...']: 
+        if not splitted in punctuation: 
             new_str = new_str + splitted + " "
     unigrams = new_str[:-1]
 
@@ -176,19 +195,18 @@ def sanitize(text):
     # bigrams
     new_str = ""
     for i in range( len(splitted_text) -1):
-        if (not splitted_text[i] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+1] in [',', '.', ';', '?', ':', '!', '...']):
+        if (not splitted_text[i] in punctuation) and (not splitted_text[i+1] in punctuation):
             new_str = new_str + splitted_text[i] + "_" + splitted_text[i+1] + " "
     bigrams = new_str[:-1]
 
     # trigrams
     new_str = ""
     for i in range( len(splitted_text) -2):
-        if (not splitted_text[i] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+1] in [',', '.', ';', '?', ':', '!', '...']) and (not splitted_text[i+2] in [',', '.', ';', '?', ':', '!', '...']):
+        if (not splitted_text[i] in punctuation) and (not splitted_text[i+1] in punctuation) and (not splitted_text[i+2] in [',', '.', ';', '?', ':', '!', '...']):
             new_str = new_str + splitted_text[i] + "_" + splitted_text[i+1] + "_" + splitted_text[i+2] + " "
     trigrams = new_str[:-1]
 
-    return parsed_text, unigrams, bigrams, trigrams
-
+    print (temp, '\n', parsed_text,'\n', unigrams,'\n', bigrams,'\n', trigrams, '\n')
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -202,8 +220,8 @@ if __name__ == "__main__":
             content = json.loads(line)
             comment = content['body']
 
-            print (sanitize(comment))
-            print()
+            sanitize(comment)
+            # print()
             # pp.pprint(comment)
 
     # This is the Python main function.
