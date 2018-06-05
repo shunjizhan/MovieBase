4ee62ce84bb314079a3654a42d0734045497b4b3
Don <donlee940125@gmail.com>
2018-06-05 14:35:50 -0700

    finished parquet work

--- reddit_model.py
+++ reddit_model.py
@@ -19,13 +19,20 @@ if __name__ == "__main__":
     sc.addPyFile("cleantext.py")
 
     # Task 1
-    comments = sqlContext.read.json("comments-minimal-small.json")    # data_frame for comments
-    submissions = sqlContext.read.json("submissions-small.json")        # data_frame for submissions
-    labeled_data = sqlContext.read.load("labeled_data.csv", format="csv", sep=",", inferSchema="true", header="true")
+    # comments = sqlContext.read.json("comments-minimal.json")    # data_frame for comments
+    # submissions = sqlContext.read.json("submissions.json")        # data_frame for submissions
+    # labeled_data = sqlContext.read.load("labeled_data.csv", format="csv", sep=",", inferSchema="true", header="true")
 
-    comments.show()
-    print('*' * 50)
-    submissions.show()
+    # comments.write.parquet("comments.parquet")
+    # submissions.write.parquet("submissions.parquet")
+    # labeled_data.write.parquet("labeled_data.parquet")
+
+    comments = sqlContext.read.parquet("comments.parquet")
+    submissions = sqlContext.read.parquet("submissions.parquet")
+    labeled_data = sqlContext.read.parquet("labeled_data.parquet")
+    # comments.show()
+    # print('*' * 50)
+    # submissions.show()
 
     # Task 2
     comments.createOrReplaceTempView("comments_view")           # Register the df as a SQL temporary view
@@ -35,7 +42,7 @@ if __name__ == "__main__":
 
     # labelded_comments = sqlContext.sql("  SELECT id, body, labeldem, labelgop, labeldjt FROM comments_view, labeled_data_view WHERE id = Input_id LIMIT 10")
     labelded_comments = sqlContext.sql("SELECT id, body, labeldem, labelgop, labeldjt FROM comments_view, labeled_data_view WHERE id = Input_id")
-    labelded_comments.show()
+    # labelded_comments.show()
     # labelded_comments.printSchema()
     
     # Task 3
