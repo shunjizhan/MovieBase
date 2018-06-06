6af825a7910c688e8a0c8877ba2edb8a4f2fa502
Eric <zixia@umail.ucsb.edu>
2018-06-05 20:17:10 -0700

    add model

--- reddit_model.py
+++ reddit_model.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
     # submissions.show()
 
     # Task 2
+    '''
     comments.createOrReplaceTempView("comments_view")           # Register the df as a SQL temporary view
     submissions.createOrReplaceTempView("submissions_view")
     labeled_data.createOrReplaceTempView("labeled_data_view")
@@ -85,7 +86,6 @@ if __name__ == "__main__":
     labelded_comments = labelded_comments.withColumn("tokens", udfGramsToToken("grams"))
     labelded_comments.show()
 
-
     # Task 6 A
     cv = CountVectorizer(inputCol='tokens', outputCol='features', binary=True, minDF=5)
 
@@ -103,3 +103,57 @@ if __name__ == "__main__":
 
     result.show()
 
+    # Task 7
+    pos = result.drop('id', 'body', 'labeldjt', 'grams', 'tokens', 'neg')
+    neg = result.drop('id', 'body', 'labeldjt', 'grams', 'tokens', 'pos')
+    pos = pos.toDF('features', 'label')
+    neg = neg.toDF('features', 'label')
+    # pospos.withColumnRenamed("features", "pos").withColumnRenamed("features", "label")
+    # neg.withColumnRenamed("features", "neg").withColumnRenamed("features", "label")
+    pos.show()
+    neg.show()
+    # Bunch of imports (may need more)
+    from pyspark.ml.classification import LogisticRegression
+    from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+    from pyspark.ml.evaluation import BinaryClassificationEvaluator
+    # Initialize two logistic regression models.
+    # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+    poslr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10)
+    neglr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10)
+    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+    posEvaluator = BinaryClassificationEvaluator()
+    negEvaluator = BinaryClassificationEvaluator()
+    # There are a few parameters associated with logistic regression. We do not know what they are a priori.
+    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+    # We will assume the parameter is 1.0. Grid search takes forever.
+    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+    # We initialize a 5 fold cross-validation pipeline.
+    posCrossval = CrossValidator(
+        estimator=poslr,
+        evaluator=posEvaluator,
+        estimatorParamMaps=posParamGrid,
+        numFolds=5)
+    negCrossval = CrossValidator(
+        estimator=neglr,
+        evaluator=negEvaluator,
+        estimatorParamMaps=negParamGrid,
+        numFolds=5)
+    # Although crossvalidation creates its own train/test sets for
+    # tuning, we still need a labeled test set, because it is not
+    # accessible from the crossvalidator (argh!)
+    # Split the data 50/50
+    posTrain, posTest = pos.randomSplit([0.5, 0.5])
+    negTrain, negTest = neg.randomSplit([0.5, 0.5])
+    # Train the models
+    print("Training positive classifier...")
+    posModel = posCrossval.fit(posTrain)
+    print("Training negative classifier...")
+    negModel = negCrossval.fit(negTrain)
+
+    # Once we train the models, we don't want to do it again. We can save the models and load them again later.
+    posModel.save("www/pos.model")
+    negModel.save("www/neg.model")
+    '''
+
+    
