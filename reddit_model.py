a38728b05c8883535f371f46248b829173af0e55
Eric <zixia@umail.ucsb.edu>
2018-06-06 17:19:22 -0700

    task 10 3 queries

--- reddit_model.py
+++ reddit_model.py
@@ -8,12 +8,18 @@ from cleantext import sanitize
 from pyspark.sql.functions import udf
 from pyspark.sql.types import ArrayType, StringType, IntegerType, DoubleType
 from pyspark.ml.tuning import CrossValidatorModel
+from pyspark import rdd
 
 
 def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
+
+
+# def intersect(row):
+#     row = [x.lower() for x in row.split()]
+#     return True if set(row).intersection(set(words)) else False
 
 
 def str_to_tokens(comments):
@@ -154,9 +160,10 @@ if __name__ == "__main__":
     # # Once we train the models, we don't want to do it again. We can save the models and load them again later.
     # posModel.save("www/pos.model")
     # negModel.save("www/neg.model")
-    
+
     # print("done")
 
+    '''
     # Task 8
     # comments.printSchema()
     # submissions.printSchema()
@@ -194,13 +201,31 @@ if __name__ == "__main__":
     result_neg = None
 
     final_result.printSchema()
-    final_result.show()
+    # final_result.show()
     print ("done task 9")
     final_result.write.parquet("task9.parquet")
-
+    '''
     # Task 10 可以跑 但是show不出来
-    # final_result.createOrReplaceTempView('final_res')
-    
-    # ten_1 = sqlContext.sql("SELECT count(*) FROM posres WHERE pos = 1")
-    # ten_2 = sqlContext.sql("SELECT DATE(FROM_UNIXTIME(retrieved_on)), COUNT(pos)/COUNT(pos)+COUNT(neg) as 'Pos percent', COUNT(neg)/COUNT(pos)+COUNT(neg) as 'Neg percent', FROM final_res GROUP BY DATE(FROM_UNIXTIME(retrieved_on))")
-    # ten_2.show()
+
+    final_result = sqlContext.read.parquet("task9.parquet")
+    final_result.createOrReplaceTempView('final_res')
+
+    # TASK 10 #1
+    ten_1 = sqlContext.sql('SELECT title as submissions, COUNT(pos)/(COUNT(neg)+COUNT(pos)) as pos_percent, COUNT(neg)/(COUNT(neg)+COUNT(pos)) as neg_percent FROM final_res GROUP BY title')
+
+    # TASK 10 #2
+    ten_2 = sqlContext.sql('SELECT FROM_UNIXTIME(retrieved_on, "YYYY-MM-dd") as which_date, COUNT(pos)/(COUNT(neg)+COUNT(pos)) as pos_percent, COUNT(neg)/(COUNT(neg)+COUNT(pos)) as neg_percent FROM final_res GROUP BY FROM_UNIXTIME(retrieved_on, "YYYY-MM-dd")')
+
+    # TASK 10 #3
+    states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+    ten_3 = sqlContext.sql('SELECT state, COUNT(pos)/(COUNT(neg)+COUNT(pos)) as pos_percent, COUNT(neg)/(COUNT(neg)+COUNT(pos)) as neg_percent FROM final_res GROUP BY state')
+    ten_3 = ten_3.where(ten_3.state.isin(states))
+
+    ten_1.show()
+    ten_2.show()
+    ten_3.show()
+
+    # TASK 10 #4
+    # ten_4
+
+
