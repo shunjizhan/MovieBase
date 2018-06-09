cec52e5eefba4ff54846786044bb595208983e5e
Eric <zixia@umail.ucsb.edu>
2018-06-08 19:14:30 -0700

    add csv file

--- reddit_model.py
+++ reddit_model.py
@@ -164,45 +164,45 @@ if __name__ == "__main__":
     # print("done")
 
     
-    # Task 8
-    # comments.printSchema()
-    # submissions.printSchema()
-    all_comments = sqlContext.sql("SELECT c.id, c.retrieved_on, body, c.score as comment_score, s.score as story_score, title, c.author_flair_text as state FROM comments_view c, submissions_view s where SUBSTR(link_id, 4) = s.id and not body like '%&gt%'")
-    # all_comments.show(50)
+    # # Task 8
+    # # comments.printSchema()
+    # # submissions.printSchema()
+    # all_comments = sqlContext.sql("SELECT c.id, c.retrieved_on, body, c.score as comment_score, s.score as story_score, title, c.author_flair_text as state FROM comments_view c, submissions_view s where SUBSTR(link_id, 4) = s.id and not body like '%&gt%'")
+    # # all_comments.show(50)
 
-    posModel = CrossValidatorModel.load("www/pos.model")
-    negModel = CrossValidatorModel.load("www/neg.model")
+    # posModel = CrossValidatorModel.load("www/pos.model")
+    # negModel = CrossValidatorModel.load("www/neg.model")
 
-    # Task 9
-    udfValueToGrams = udf(sanitize, ArrayType(StringType()))
-    all_comments = all_comments.withColumn("grams", udfValueToGrams("body"))
+    # # Task 9
+    # udfValueToGrams = udf(sanitize, ArrayType(StringType()))
+    # all_comments = all_comments.withColumn("grams", udfValueToGrams("body"))
 
-    udfGramsToToken = udf(str_to_tokens, ArrayType(StringType()))
-    all_comments = all_comments.withColumn("tokens", udfGramsToToken("grams"))
+    # udfGramsToToken = udf(str_to_tokens, ArrayType(StringType()))
+    # all_comments = all_comments.withColumn("tokens", udfGramsToToken("grams"))
 
-    all_comments = model.transform(all_comments)
+    # all_comments = model.transform(all_comments)
 
-    result_pos = posModel.transform(all_comments)
-    prob_to_pos = udf(pos_binary, IntegerType())
-    result_pos = result_pos.withColumn("pos", prob_to_pos("probability"))
-    result_pos = result_pos.select('id', 'retrieved_on', 'comment_score', 'story_score', 'title', 'state', 'features', 'pos')
+    # result_pos = posModel.transform(all_comments)
+    # prob_to_pos = udf(pos_binary, IntegerType())
+    # result_pos = result_pos.withColumn("pos", prob_to_pos("probability"))
+    # result_pos = result_pos.select('id', 'retrieved_on', 'comment_score', 'story_score', 'title', 'state', 'features', 'pos')
 
-    result_neg = negModel.transform(result_pos)
-    prob_to_neg = udf(neg_binary, IntegerType())
-    result_neg = result_neg.withColumn("neg", prob_to_neg("probability"))
-    final_result = result_neg.select('id', 'retrieved_on', 'comment_score', 'story_score', 'title', 'state', 'pos', 'neg')
+    # result_neg = negModel.transform(result_pos)
+    # prob_to_neg = udf(neg_binary, IntegerType())
+    # result_neg = result_neg.withColumn("neg", prob_to_neg("probability"))
+    # final_result = result_neg.select('id', 'retrieved_on', 'comment_score', 'story_score', 'title', 'state', 'pos', 'neg')
 
-    all_comments = None
-    comments = None
-    labeled_data = None
-    submissions = None
-    result_pos = None
-    result_neg = None
+    # all_comments = None
+    # comments = None
+    # labeled_data = None
+    # submissions = None
+    # result_pos = None
+    # result_neg = None
 
-    final_result.printSchema()
-    final_result.show()
-    print ("done task 9")
-    final_result.write.parquet("task9.parquet")
+    # final_result.printSchema()
+    # final_result.show()
+    # print ("done task 9")
+    # final_result.write.parquet("task9.parquet")
     
     
     # Task 10 可以跑 但是show不出来
@@ -224,12 +224,13 @@ if __name__ == "__main__":
     ten_4_a = sqlContext.sql('SELECT comment_score, AVG(pos) as pos_percent, AVG(neg) as neg_percent FROM  final_res  GROUP BY comment_score')
     ten_4_b = sqlContext.sql('SELECT story_score, AVG(pos) as pos_percent, AVG(neg) as neg_percent FROM final_res GROUP BY story_score')
 
-    # ten_2.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("time_data.csv")
-    # ten_3.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("state_data.csv")
-    # ten_4_b.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("submission_score.csv")
-    # ten_4_a.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("comment_score.csv")
+    ten_2.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("time_data.csv")
+    ten_3.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("state_data.csv")
+    ten_4_b.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("submission_score.csv")
+    ten_4_a.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("comment_score.csv")
     
     ten_1.createOrReplaceTempView('deli')
     deli_4a = sqlContext.sql('SELECT title FROM deli order by pos_percent desc limit 10')
-    deli_4a = sqlContext.sql('SELECT title FROM deli order by neg_percent desc limit 10')
-
+    deli_4b = sqlContext.sql('SELECT title FROM deli order by neg_percent desc limit 10')
+    # deli_4a.show(10, False)
+    # deli_4b.show(10, False)
